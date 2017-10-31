681a58d10ecb83f7d48c71d69942bf37094cc72f
Julian Lees <julian.j.lees@gmail.com>
2017-10-31 21:41:42 +1300

    Developing production environment with Heroku using embedded Tomcat 8

---
+++ src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,24 @@
+package main.java.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Julian on 31/10/2017.
+ */
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("hello heroku".getBytes());
+        out.flush();
+        out.close();
+    }
+
+}
