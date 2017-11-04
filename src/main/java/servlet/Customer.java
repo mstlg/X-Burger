b397f9200f7cabdb7e359bc11b81d08f9a3725ba
Julian Lees <julian.j.lees@gmail.com>
2017-11-05 12:49:55 +1300

    Initial development of login system

---
+++ src/main/java/servlet/Customer.java
@@ -0,0 +1,36 @@
+package main.java.servlet;
+
+/**
+ * Created by Julian on 5/11/2017.
+ */
+public class Customer {
+
+    int customer_id;
+    String username;
+    String email;
+    String phone_number;
+    int iterations;
+    String salt;
+
+    // Constructor for Customer object with full suite of details as pulled from the database
+    public Customer(int customer_id, String username, String email, String phone_number, int iterations, String salt) {
+        this.customer_id = customer_id;
+        this.username = username;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.iterations = iterations;
+        this.salt = salt;
+    }
+
+    // Constructor for "prospective" customer with full suite of details (minus customer_id which will be assigned on registration
+    public Customer(String username, String email, String phone_number, int iterations, String salt) {
+        this.username = username;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.iterations = iterations;
+        this.salt = salt;
+    }
+
+
+
+}
---
+++ src/main/java/servlet/LoginValidation.java
@@ -0,0 +1,25 @@
+package main.java.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Julian on 5/11/2017.
+ */
+public class LoginValidation extends HttpServlet {
+
+    // If this servlet is accessed via HTTP GET: return the user to the login page (.jsp)
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    // This servlet should only be accessed via post. If posted, access the customers details from the API and validate credentials and return response depending on validation process
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
